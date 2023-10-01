0bd73877d4c9c8c15c7e4c625225a3bfa8e2a06a
asemahle <aidan.mahler13@gmail.com>
2023-09-30 23:56:19 -0400

    Win condition

---
+++ scripts/ParkingSpace.gd
@@ -0,0 +1,68 @@
+@tool
+extends Node2D
+
+@export var spaces_wide: int:
+	set(value):
+		spaces_wide = value
+		init_lines()
+@export var spaces_tall: int:
+	set(value):
+		spaces_tall = value
+		init_lines()
+@export var space_width: int:
+	set(value):
+		space_width = value
+		init_lines()
+@export var space_height: int:
+	set(value):
+		space_height = value
+		init_lines()
+
+var lines: Array[Line2D] = []
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	init_lines()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func init_lines():
+	# destroy any lines that already exist
+	for line in lines:
+		remove_child(line)
+	lines = []
+	
+	# create lines
+	for x in range(0, spaces_wide):
+		for y in range(0, spaces_tall):
+			var offset = Vector2(x * space_width, y * space_height)
+			var top_left = Vector2(0, 0) + offset
+			var bottom_left = Vector2(0, space_height) + offset
+			var bottom_right = Vector2(space_width, space_height) + offset
+			var top_right = Vector2(space_width, 0) + offset
+			
+			if y == 0 || y < spaces_tall - 1:
+				var line = Line2D.new()
+				if x == 0: 
+					line.add_point(top_left)
+				line.add_point(bottom_left)
+				line.add_point(bottom_right)
+				line.add_point(top_right)
+				lines.append(line)
+			else:
+				if x == 0:
+					var left_line = Line2D.new()
+					left_line.add_point(top_left)
+					left_line.add_point(bottom_left)
+					lines.append(left_line)
+				var right_line = Line2D.new()
+				right_line.add_point(top_right)
+				right_line.add_point(bottom_right)
+				lines.append(right_line)
+	
+	#add lines
+	for line in lines:
+		add_child(line)
--- scripts/PlayerCar.gd
+++ scripts/PlayerCar.gd
@@ -2,11 +2,38 @@ extends "res://scripts/Car.gd"
 
 @export var max_steer_deg : float = 55.0
 
+@onready var parking_detector_1 = $ParkingDetector1
+@onready var parking_detector_2 = $ParkingDetector2
+@onready var parking_detector_3 = $ParkingDetector3
+@onready var parking_detector_4 = $ParkingDetector4
+@onready var sprite = $Sprite2D
+
+var level_manager : LevelManager
+
 var mouse_active_timer : float = 0.0
 const MOUSE_ACTIVE_TIME : float = 0.75
 const MOUSE_STEER_TOLERANCE_DEG : float = 1.0
 
+var in_target_space : bool = false
+var parked_active_timer : float = 0.0
+const PARKED_TIMER : float = 0.2
+
+var has_won : bool = false
+var has_won_active_timer : float = 0.0
+var HAS_WON_TIMER : float = 2.0
+
+func _ready():
+	level_manager = get_node("/root/LevelManager")
+
 func _process(delta : float) -> void:
+	if has_won:
+		has_won_active_timer += delta
+		var scale = pow(1 + has_won_active_timer, 2)
+		sprite.scale = Vector2(scale, scale)
+		if has_won_active_timer > HAS_WON_TIMER:
+			level_manager.change_level(level_manager.current_level + 1)
+		return
+	
 	var mouse_steer := 0.0
 	if mouse_active_timer > 0.0:
 		mouse_active_timer -= delta
@@ -32,8 +59,22 @@ func _process(delta : float) -> void:
 	drive_forward = maxf(drive_forward, Input.get_action_strength("drive_forward_analog"))
 	drive_reverse = maxf(drive_reverse, Input.get_action_strength("drive_reverse_analog"))
 	drive_power = drive_forward - drive_reverse
+	
+	if in_target_space and linear_velocity.length() < 3 and angular_velocity < 1:
+		parked_active_timer += delta
+		if parked_active_timer > PARKED_TIMER:
+			has_won = true
+	else:
+		parked_active_timer = 0.0
+		
 
 func _input(event : InputEvent) -> void:
 	if event is InputEventMouse:
 		mouse_active_timer = MOUSE_ACTIVE_TIME
-		print("Mouse event")
+		
+func _on_parking_detector_entered(area: Area2D) -> void:
+	var o1 = parking_detector_1.overlaps_area(area)
+	var o2 = parking_detector_2.overlaps_area(area)
+	var o3 = parking_detector_3.overlaps_area(area)
+	var o4 = parking_detector_4.overlaps_area(area)
+	in_target_space = o1 and o2 and o3 and o4
